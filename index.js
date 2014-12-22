261c989305f3922df2d028900c443eb0da2d12e9
Stephen Sawchuk <sawchuk@gmail.com>
2014-12-22 14:15:01 -0500

    don't end the stream
    'see https://github.com/GoogleCloudPlatform/gcloud-node/issues/335'

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ var through = require("through2")
 module.exports = function (start, end) {
   end += 1
   var bytesReceived = 0
+  var lastByteFound = false
 
   return through(function (chunk, enc, next) {
     bytesReceived += chunk.length
 
-    if (bytesReceived >= start) {
+    if (!lastByteFound && bytesReceived >= start) {
       if (start - (bytesReceived - chunk.length) > 0)
         chunk = chunk.slice(start - (bytesReceived - chunk.length))
 
       if (end <= bytesReceived) {
         this.push(chunk.slice(0, chunk.length - (bytesReceived - end)))
-        this.end()
+        lastByteFound = true
       } else
         this.push(chunk)
     }
