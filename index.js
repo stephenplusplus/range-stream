ebac5b1d1c34abe418bf06c1d0750caeeb290f91
Mike <reinstein.mike@gmail.com>
2022-09-24 18:49:34 -0700

    replace through2 which pure nodejs transform stream

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var through = require("through2")
+var Transform = require('stream').Transform
+
 
 module.exports = function (start, end) {
   end = typeof end === 'number' ? end + 1 : Infinity
@@ -6,20 +7,20 @@ module.exports = function (start, end) {
   var bytesReceived = 0
   var lastByteFound = false
 
-  return through(function (chunk, enc, next) {
-    bytesReceived += chunk.length
+  return new Transform({
+    transform(chunk, enc, next) {
+      bytesReceived += chunk.length
+      if (!lastByteFound && bytesReceived >= start) {
+        if (start - (bytesReceived - chunk.length) > 0)
+          chunk = chunk.slice(start - (bytesReceived - chunk.length))
 
-    if (!lastByteFound && bytesReceived >= start) {
-      if (start - (bytesReceived - chunk.length) > 0)
-        chunk = chunk.slice(start - (bytesReceived - chunk.length))
-
-      if (end <= bytesReceived) {
-        this.push(chunk.slice(0, chunk.length - (bytesReceived - end)))
-        lastByteFound = true
-      } else
-        this.push(chunk)
+        if (end <= bytesReceived) {
+          this.push(chunk.slice(0, chunk.length - (bytesReceived - end)))
+          lastByteFound = true
+        } else
+          this.push(chunk)
+      }
+      next()
     }
-
-    next()
   })
 }
